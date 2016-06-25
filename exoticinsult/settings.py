fc45e338e8b4d893dc3563a52e3b20405046fb4b
ETinLV <ericturnernv@gmail.com>
2016-06-25 08:08:34 -0700

    working static issue

--- exoticinsult/settings.py
+++ exoticinsult/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
