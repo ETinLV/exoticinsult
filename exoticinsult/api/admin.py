81791a019f57b0425f04d6e2f6397c75713e5959
ETinLV <ericturnernv@gmail.com>
2016-06-25 11:25:28 -0700

    starting styling

--- exoticinsult/api/admin.py
+++ exoticinsult/api/admin.py
@@ -1,7 +1,71 @@
 from django.contrib import admin
-from django.apps import apps
-app = apps.get_app_config('api')
-# Register your models here.
+from django.contrib.admin import sites as admin_sites
 
-for model_name, model in app.models.items():
-    admin.site.register(model)
+from django.apps import apps
+
+
+def list_all_fields(model, excluded_fields=None):
+  """Creates a custom admin list display showing all the model's fields.
+
+  Args
+  ----
+  model : django.db.models.Model
+      The model to create the admin interface for.
+  excluded_fields : Iterable[str]
+      An iterable of field name strings to exclude.
+
+  Returns
+  -------
+  Tuple[django.db.models.Model, django.contrib.admin.ModelAdmin]
+      A 2 tuple containing the model and the generated admin interface.
+
+  """
+  if excluded_fields:
+    excluded_fields = set(excluded_fields)
+    fields = [f.name for f in model._meta.fields if f not in excluded_fields]
+  else:
+    fields = [f.name for f in model._meta.fields]
+
+  class Admin(admin.ModelAdmin):
+    list_display = fields
+
+  return (model, Admin)
+
+
+def register(model, admin_class=None, **options):
+  """Register a Model and optional ModelAdmin.
+
+  Args
+  ----
+  model : django.db.models.Model
+      The model to register.
+  admin_class : Optional[django.contrib.admin.ModelAdmin]
+      The admin class to use.
+  **options
+      Keyword arguments passed to the admin_class.
+
+  """
+  try:
+    admin.site.register(model, admin_class, **options)
+  except admin_sites.AlreadyRegistered:
+    pass
+
+
+def register_all_models(app_name):
+  """Register all Models for a given app.
+
+  Args
+  ----
+  app_name : str
+      The name of the app to register.
+
+  """
+  app = apps.get_app_config(app_name)
+  models = app.get_models()
+
+  for model in models:
+    register(*list_all_fields(model))
+
+
+# Register the models
+register_all_models('api')
--- exoticinsult/api/models.py
+++ exoticinsult/api/models.py
@@ -4,9 +4,16 @@ from django.db import models
 
 class Insult(models.Model):
     phrase = models.TextField()
+    pronunciation = models.TextField()
     language = models.CharField(max_length=255)
     translation = models.TextField()
+
+    def __str__(self):
+        return self.phrase
 
 class Day(models.Model):
     date = models.DateField(auto_now_add=True, primary_key=True)
     insult = models.ForeignKey(Insult, related_name='days')
+
+    def __str__(self):
+        return self.date
