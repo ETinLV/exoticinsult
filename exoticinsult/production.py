c0c0e7021a32d22f355d209a00e9092549d82e4a
ETinLV <ericturnernv@gmail.com>
2016-06-27 15:24:20 -0700

    debug error

--- exoticinsult/production.py
+++ exoticinsult/production.py
@@ -5,7 +5,7 @@ from exoticinsult.settings import *
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-DEBUG=False
+DEBUG=True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
