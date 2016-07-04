9033958ddeef41028f36f1466cd2f7e935b1e0ef
ETinLV <ericturnernv@gmail.com>
2016-07-04 15:40:01 -0700

    remove whitenoise

--- exoticinsult/settings.py
+++ exoticinsult/settings.py
@@ -143,5 +143,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
